a51eb1bdb32a6870710937db0e136a86d9231442
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2024-09-08 01:43:57 -0700

    hope

--- scripts/enter.js
+++ scripts/enter.js
@@ -1,3 +1,9 @@
 setTimeout(() => {
-    window.location.href = '/opening.html';
+    // Check if we're already on the index page
+    if (window.location.pathname.endsWith('SBA/') || window.location.pathname.endsWith('SBA/index.html')) {
+        window.location.href = 'opening.html';
+    } else {
+        // If we're on another page, go back to the index
+        window.location.href = 'index.html';
+    }
 }, 6000);
